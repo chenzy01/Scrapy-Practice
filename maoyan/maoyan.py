c2a4628ee26a8ca2b12bedbe4fc638daa9e3e0e2
chenzy01 <511590474@qq.com>
2018-08-30 23:19:11 +0800

    Scrapy maoyan film

---
+++ maoyan/maoyan.py
@@ -0,0 +1,38 @@
+import re
+import requests
+
+def get_one_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/68.0.3440.75 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_one_page(html):
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>'
+        '.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip() + item[6].strip()
+        }
+
+
+def main():
+    url = 'http://maoyan.com/board/4'
+    html = get_one_page(url)
+    response = parse_one_page(html)
+    for result in response:
+        print(result)
+
+main()
