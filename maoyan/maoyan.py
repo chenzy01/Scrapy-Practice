f529f7a91c399e96d3e894d399d959812949d20f
chenzy01 <511590474@qq.com>
2018-08-31 23:18:42 +0800

    update scrapy maoyan film

--- maoyan/maoyan.py
+++ maoyan/maoyan.py
@@ -1,22 +1,31 @@
+import json
 import re
 import requests
+from requests.exceptions import RequestException
+import time
 
+
+#获取一页的源码
 def get_one_page(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/68.0.3440.75 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.text
-    return None
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
 
-
+#正则解析网页，提取电影信息
 def parse_one_page(html):
     pattern = re.compile(
         '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>'
         '.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',re.S)
     items = re.findall(pattern, html)
+    #使用yield关键字，函数成为迭代器
     for item in items:
         yield {
             'index': item[0],
@@ -27,12 +36,22 @@ def parse_one_page(html):
             'score': item[5].strip() + item[6].strip()
         }
 
+#写入文件，json格式
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        print(type(json.dumps(content)))
+        #用json序列化存储数据
+        f.write(json.dumps(content, ensure_ascii=False)+'\n')
 
-def main():
-    url = 'http://maoyan.com/board/4'
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
     html = get_one_page(url)
-    response = parse_one_page(html)
-    for result in response:
-        print(result)
+    for item in parse_one_page(html):
+        #传入参数为列表
+        write_to_file(item)
 
-main()
+
+if __name__== '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
